8a64f7df2276be30d54184040293026514e3c564
Mustafe-Abdirahman <khadar3021@gmail.com>
2024-12-25 09:10:16 +0300

    Last Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline  bg-red-400">Hello</h1>;
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Price from "./pages/Price";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Price />} />
+      </Routes>
+      <Header />
+    </BrowserRouter>
+  );
 }
+
+export default App;
